ef3467b488d85caeea9a1cf683b4e0d965e97426
Waseef Akhtar <waseef@degoo.com>
2022-01-25 02:39:46 +0500

    Create buildSrc module and add dependencies

---
+++ buildSrc/src/main/java/DaggerHilt.kt
@@ -0,0 +1,6 @@
+object DaggerHilt {
+    const val version = "2.4.0"
+    const val hiltAndroid = "com.google.dagger:hilt-android:$version"
+    const val hiltCompiler = "com.google.dagger:hilt-android-compiler:$version"
+
+}
