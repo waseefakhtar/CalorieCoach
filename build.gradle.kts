520a9ef6ca58e11d87b443a04e56d94bc6077f32
Waseef Akhtar <waseef@degoo.com>
2022-01-25 02:58:58 +0500

    Migrate Gradle Groovy to Gradle Kotlin DSL, add base-module.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,15 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:7.0.3"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10"
+        classpath(Build.androidBuildTools)
+        classpath(Build.hiltAndroidGradlePlugin)
+        classpath(Build.kotlinGradlePlugin)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
