85c4aad852123dece233abb6cb872ba78f5364ac
Waseef Akhtar <waseef@degoo.com>
2022-01-27 01:45:08 +0500

    Add core-ui module, create Dimensions class and WelcomeScreen, move strings.xml to core

---
+++ onboarding/onboarding_presentation/src/main/java/com/waseefakhtar/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -0,0 +1,34 @@
+package com.waseefakhtar.onboarding_presentation.welcome
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.waseefakhtar.core.R
+
+@Composable
+fun WelcomeScreen() {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.welcome_text),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h1
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
