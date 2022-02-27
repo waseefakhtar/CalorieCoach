3964deb28eca2ee6049542980e6efd145109c20c
Waseef Akhtar <waseef@degoo.com>
2022-02-27 17:24:17 +0500

    Implement NutrientGoal screen, set up Retrofit via DI

--- app/src/main/java/com/waseefakhtar/caloriecoach/MainActivity.kt
+++ app/src/main/java/com/waseefakhtar/caloriecoach/MainActivity.kt
@@ -19,6 +19,7 @@ import com.waseefakhtar.onboarding_presentation.age.AgeScreen
 import com.waseefakhtar.onboarding_presentation.gender.GenderScreen
 import com.waseefakhtar.onboarding_presentation.goal.GoalScreen
 import com.waseefakhtar.onboarding_presentation.height.HeightScreen
+import com.waseefakhtar.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.waseefakhtar.onboarding_presentation.weight.WeightScreen
 import com.waseefakhtar.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -61,7 +62,10 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                         composable(Route.NUTRIENT_GOAL) {
-
+                            NutrientGoalScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
                         }
                         composable(Route.ACTIVITY) {
                             ActivityScreen(
---
+++ onboarding/onboarding_domain/src/main/java/com/waseefakhtar/onboarding_domain/di/OnboardingDomainModule.kt
@@ -0,0 +1,21 @@
+package com.waseefakhtar.onboarding_domain.di
+
+import com.waseefakhtar.onboarding_domain.use_case.ValidateNutrients
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object OnboardingDomainModule {
+
+    @Provides
+    @ViewModelScoped
+    fun provideValidateNutrientsUseCase(): ValidateNutrients {
+        return ValidateNutrients()
+    }
+}
