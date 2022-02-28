5c46734ff922974d486674b64bed43e27896fab5
Waseef Akhtar <waseef@degoo.com>
2022-03-01 01:03:47 +0500

    Provide TrackerRepository via DI

--- tracker/tracker_data/src/main/java/com/waseefakhtar/tracker_data/di/TrackerDataModule.kt
+++ tracker/tracker_data/src/main/java/com/waseefakhtar/tracker_data/di/TrackerDataModule.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.room.Room
 import com.waseefakhtar.tracker_data.local.TrackerDatabase
 import com.waseefakhtar.tracker_data.remote.OpenFoodApi
+import com.waseefakhtar.tracker_data.repository.TrackerRepositoryImpl
+import com.waseefakhtar.tracker_domain.repository.TrackerRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -49,5 +51,17 @@ object TrackerDataModule {
             TrackerDatabase::class.java,
             "tracker_db"
         ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTrackerRepository(
+        api: OpenFoodApi,
+        db: TrackerDatabase
+    ): TrackerRepository {
+        return TrackerRepositoryImpl(
+            dao = db.dao,
+            api = api
+        )
     }
 }
